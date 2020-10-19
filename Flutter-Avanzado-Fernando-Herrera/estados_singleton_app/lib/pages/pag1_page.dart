e53139272897d29a19423451c3ae0c0db9359d5f
fernandomiras1 <fernando.miras.pc@gmail.com>
2020-10-19 20:36:30 -0300

    flutter  terminado app votos io

---
+++ Flutter-Avanzado-Fernando-Herrera/estados_singleton_app/lib/pages/pag1_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class Page1Page extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Page1Page'),
+     ),
+   );
+  }
+}
